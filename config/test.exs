b8cf49d079eb428774983a1d38a633afa85948e7
Jonathan Worek <jworek@gmail.com>
2016-12-16 14:03:47 -0500

    adds files that have been forgotten

--- config/test.exs
+++ config/test.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :rumbl, Rumbl.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "NhbGPDKTSRIm9stYdUQH5Rpue",
   database: "rumbl_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
